5210c5199cae61b0c5b68a8945984ba6d6c5aec6
aahaidar01 <alihaidar.01.ah@gmail.com>
2026-01-17 20:04:54 +0000

    [Edit] Added path to .world and .urdf files into setup data_files

--- robot_simulation/setup.py
+++ robot_simulation/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob 
+import os
 
 package_name = 'robot_simulation'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +29,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'rl_agent = robot_simulation.rl_agent:main',
         ],
     },
 )
